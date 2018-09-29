e9c6ea4ddf5662b34aad517427accf8218e180ad
jbrolin <brolin.jonas@gmail.com>
2018-09-29 15:45:38 +0200

    implemented caching at client, only call api if search not in results.

--- src/App.js
+++ src/App.js
@@ -2,150 +2,148 @@ import React, { Component } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_HPP = '10';
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const DEFAULT_QUERY = "redux";
+const DEFAULT_HPP = "10";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
 const PATH_SEARCH = "/search";
 const PARAM_SEARCH = "query=";
 const PARAM_PAGE = "page=";
 const PARAM_HPP = "hitsPerPage=";
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}&${PARAM_HPP}${DEFAULT_HPP}`;
-console.log("#####", url);
-
-
+//const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}&${PARAM_HPP}${DEFAULT_HPP}`;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //list: list,
-      //textFields: textFields,
-      result: null,
-      searchTerm: DEFAULT_QUERY,
-
+     
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY
     };
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
-   
+
     this.onSearchChange = this.onSearchChange.bind(this);
-    this.onDismiss=this.onDismiss.bind(this);
-    this.onSearchSubmit=this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
 
-  onSearchSubmit(event){
+  needsToSearchTopStories(searchTerm){
+    return !this.state.results[searchTerm];
+  }
+
+  onSearchSubmit(event) {
     const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)){
+      this.fetchSearchTopStories(searchTerm);
+    }
     event.preventDefault(); // prevent default behaviour in this case do not reload page
   }
   setSearchTopStories(result) {
-    const {hits,page} = result;
-    console.log("hits",hits, page);
-    const oldHits = page !== 0
-      ? this.state.result.hits
-      : [];
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
 
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ];
+    const oldHits = results && results[searchKey] 
+    ? results[searchKey].hits 
+    : [];
 
-    this.setState({ 
-      result :{hits: updatedHits, page}
-      });
+    const updatedHits = [...oldHits, ...hits];
+
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
   }
-
-
 
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
 
-  fetchSearchTopStories(searchTerm, page=0){
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    console.log("fetch searched items when not previosly in results");
+    fetch(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
-
   }
 
   componentDidMount() {
     const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
 
-
   onDismiss(id) {
-    console.log("Dismiss"+id);
+    const {searchKey, results} = this.state;
+    const {hits, page}= results[searchKey];
+
     const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
     this.setState({
-      result: { ...this.state.result, hits: updatedHits }  
+      results: { 
+        ...results,
+        [searchKey]: {hits:updatedHits, page}
+      }
     });
   }
 
-  
-  
-
-
-
   render() {
-    const { searchTerm, result } = this.state; // destructing values from this.state
-    const page = (result && result.page) || 0;
-    console.log("get", this.state, result);
+    const { searchTerm, results, searchKey } = this.state; // destructing values from this.state
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
 
-    //if (!result) { return null; }
-   
     return (
       <div className="page">
-      
-      { result && /* we use ' {result && ... }' which will show the table if result has data if not it shows nothing (called conditional rendering) */
-      <Table
-      list={result.hits}
-      pattern={searchTerm}
-      onDismiss={this.onDismiss}
-      />
-      
-      }
-      
-           <div className="interactions">
+       
+          <Table
+            list={list}
+            onDismiss={this.onDismiss}
+          />
+        
+
+        <div className="interactions">
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
             onSubmit={this.onSearchSubmit}
             onReset={this.onReset}
           >
-          Search
+            Search
           </Search>
 
-          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page+1)}>
-          Show more
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+          >
+            Show more
           </Button>
-        
-
         </div>
-        
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, onSubmit, children }) =>
+const Search = ({ value, onChange, onSubmit, children }) => (
   <form onSubmit={onSubmit}>
     Filter:
-          <input
+    <input
       type="text"
-      value={value} // using the string as value in the html-form makes it a controlled component, 
-      // that is the string is now the "single source of truth" 
+      value={value} // using the string as value in the html-form makes it a controlled component,
+      // that is the string is now the "single source of truth"
       onChange={onChange}
     />
-    <button type="submit">
-    {children}
-    </button>
+    <button type="submit">{children}</button>
   </form>
+);
 
-const Table = ({ list, onDismiss }) =>
-
+const Table = ({ list, onDismiss }) => (
   <div className="table">
     {list.map(item => (
       <div key={item.objectID} className="table-row">
@@ -154,32 +152,26 @@ const Table = ({ list, onDismiss }) =>
             {item.title}
           </a>
         </span>
-        <span className="midColumn">
-          {item.author}
-        </span>
-        <span className="smallColumn">
-          {item.num_comments}
-        </span>
-        <span className="smallColumn">
-          {item.points}
-        </span>
-        <span style={{ width: '10%' }}>
-          <Button onClick={() => onDismiss(item.objectID)} className="button-inline">
+        <span className="midColumn">{item.author}</span>
+        <span className="smallColumn">{item.num_comments}</span>
+        <span className="smallColumn">{item.points}</span>
+        <span style={{ width: "10%" }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
             <b>Do</b> Dismiss
-              </Button>
+          </Button>
         </span>
       </div>
     ))}
   </div>
+);
 
-const Button = ({ onClick, className = '', children }) =>
-  <button
-    onClick={onClick}
-    className={className}
-    type="button"
-  >
+const Button = ({ onClick, className = "", children }) => (
+  <button onClick={onClick} className={className} type="button">
     {children}
   </button>
-
+);
 
 export default App;
