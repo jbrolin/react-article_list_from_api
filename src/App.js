33adf4413d571ed004e81abc6521fe70690ddc59
jbrolin <brolin.jonas@gmail.com>
2018-09-30 14:57:45 +0200

    refactoring to use axios library instead of native fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 //import logo from "./logo.svg";
 import "./App.css";
 
@@ -68,11 +69,10 @@ class App extends Component {
 
   fetchSearchTopStories(searchTerm, page = 0) {
     console.log("fetch searched items when not previosly in results");
-    fetch(
+    axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
+      .then(result => this.setSearchTopStories(result.data))
       .catch(error => this.setState({error}));
   }
 
